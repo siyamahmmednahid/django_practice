490ba5224e71d8e0bb731ea612706d763a056156
Siyam Ahmmed Nahid <siyamahmmednahid@Siyams-iMac.local>
2022-09-23 02:10:50 +0600

    Implement login and logout

--- src/auth_app/urls.py
+++ src/auth_app/urls.py
@@ -6,4 +6,6 @@ app_name = 'auth_app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- src/auth_app/views.py
+++ src/auth_app/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from auth_app import forms
 
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def index(request):
     homeDict = {'title': 'Home Page'}
     return render(request, 'auth_app/index.html', context=homeDict)
 
 
+# For registration
 def register(request):
     registered = False
     if request.method == 'POST':
@@ -24,10 +30,35 @@ def register(request):
                 userInfo.profile_pic = request.FILES['profile_pic']
             userInfo.save()
             registered = True
-
-
     else:
         userForm = forms.UserForm()
         userInfoForm = forms.UserInfoForm()
     registerDict = {'title': 'Register Page', 'userForm': userForm, 'userInfoForm': userInfoForm, 'registered': registered}
     return render(request, 'auth_app/register.html', context=registerDict)
+
+
+# For login
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('auth_app:index'))
+            else:
+                return HttpResponse('Account not active')
+        else:
+            return HttpResponse('Invalid login details supplied')
+    else:
+        return render(request, 'auth_app/login.html')
+
+
+# For logout
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('auth_app:index'))
